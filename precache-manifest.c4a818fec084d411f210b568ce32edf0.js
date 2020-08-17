10bfe22949d5d12a4562934caab12ac0b3a2018e
Пользователь <vanadolgun@gmail.com>
2020-08-17 14:19:15 +0700

    Updates

---
+++ precache-manifest.c4a818fec084d411f210b568ce32edf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8842781e84267bdb61f28447b9ab396",
+    "url": "./index.html"
+  },
+  {
+    "revision": "91456a91f76bea2641bc",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "843f7bf830af138161c2",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "91456a91f76bea2641bc",
+    "url": "./static/js/2.14af4caf.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.14af4caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843f7bf830af138161c2",
+    "url": "./static/js/main.79feb0b8.chunk.js"
+  },
+  {
+    "revision": "066e1fe302588d5b0de2",
+    "url": "./static/js/runtime-main.560b22e6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90e011b6cf4d7450b2438aad81e79ee1.js"
+  "./precache-manifest.c4a818fec084d411f210b568ce32edf0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
