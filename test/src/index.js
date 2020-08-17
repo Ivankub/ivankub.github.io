173bc8c3236c599c4d0013466786a48f833090fe
Пользователь <vanadolgun@gmail.com>
2020-08-17 15:04:21 +0700

    commit_5_kak_ya_ustal_u_menya_nichego_ne_rabotaet

--- test/src/index.js
+++ test/src/index.js
@@ -8,7 +8,24 @@ import App from "./App";
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
 
+// Отправляет событие нативному клиенту
+bridge.send("VKWebAppInit", {});
+
 ReactDOM.render(<App />, document.getElementById("root"));
 if (process.env.NODE_ENV === "development") {
   import("./eruda").then(({ default: eruda }) => {}); //runtime download
 }
+
+if (bridge.supports("VKWebAppResizeWindow")) {
+  bridge.send("VKWebAppResizeWindow", {"width": 630, "height": 800});
+}
+// Sending event to client
+bridge
+  .send('VKWebAppGetEmail')
+  .then(data => {
+    // Обработка события в случае успеха
+    console.log(data.email);
+  })
+  .catch(error => {
+    // Обработка события в случае ошибки
+  });
